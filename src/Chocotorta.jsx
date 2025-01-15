a2bfb2f39e01bb3f40ea31b81922bef17d91c8ff
jonorl <39122230+jonorl@users.noreply.github.com>
2025-01-15 14:43:55 +0300

    Feat: Added fav food

---
+++ src/Chocotorta.jsx
@@ -0,0 +1,15 @@
+function DisplayChocotorta() {
+  return (
+    <div className="container">
+        <h1>Favourite Food</h1>
+        <p>Chocotorta</p>
+      <img
+        src="/public/Chocotorta-24-500x500.jpg"
+        alt="Chocotorta cake image"
+      />
+      ;
+    </div>
+  );
+}
+
+export  { DisplayChocotorta }
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import Salutation from './Greeting.jsx'
+import { DisplayChocotorta } from './Chocotorta.jsx'
 import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <DisplayChocotorta />
+    <Salutation />
   </StrictMode>,
 )
