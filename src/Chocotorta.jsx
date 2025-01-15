09e088ee21b14fdbc5a29e8c45678f57f9e9ba1b
jonorl <39122230+jonorl@users.noreply.github.com>
2025-01-15 14:58:57 +0300

    Feat: Testing JSX and HTML conversion

--- src/Chocotorta.jsx
+++ src/Chocotorta.jsx
@@ -1,15 +1,18 @@
 function DisplayChocotorta() {
   return (
-    <div className="container">
+    <>
+      <div className="container">
         <h1>Favourite Food</h1>
-        <p>Chocotorta</p>
-      <img
-        src="/public/Chocotorta-24-500x500.jpg"
-        alt="Chocotorta cake image"
-      />
-      ;
-    </div>
+        <input type="number" />
+        Something
+        <li>Chocotorta</li>
+        <img
+          src="/public/Chocotorta-24-500x500.jpg"
+          alt="Chocotorta cake image"
+        />
+      </div>
+    </>
   );
 }
 
-export  { DisplayChocotorta }
+export { DisplayChocotorta };
